a6ed257cc9550c16c48333686bf60c3806891e37
kukosek <kukys410@gmail.com>
2020-12-08 19:25:30 +0100

    trustworthy ips

--- soybeans/Security/login_guard.py
+++ soybeans/Security/login_guard.py
@@ -21,6 +21,12 @@ class LoginGuard:
                         tried_usernames varchar[]
                     );""")
         conn.commit()
+        try:
+            with open("Security/trustworthy_ips.txt", 'r') as file:
+                self.ip_whitelist = file.readlines()
+        except ValueError:
+            print("warning couldnt open trustworthy ip list")
+            self.ip_whitelist = []
 
     def get_timestamp(self):
         return datetime.now().strftime(self.bad_attempt_timestamp_format)
@@ -38,25 +44,26 @@ class LoginGuard:
 
     # Function notify login failed(ip) - increments number of bad attempts in the database for the IP
     def notify_login_failed(self, ip):
-        cur = self.conn.cursor()
-        row_id = self.get_id_by_ip(ip)
-        if row_id is None:
-            # This is the ip's first bad attempt to login
-            # Add a record to the db
+        if ip not in self.ip_whitelist:
+            cur = self.conn.cursor()
+            row_id = self.get_id_by_ip(ip)
+            if row_id is None:
+                # This is the ip's first bad attempt to login
+                # Add a record to the db
 
-            cur.execute(
-                """INSERT INTO """ + self.table_name + """ VALUES (DEFAULT, %(ip)s, 1, %(timestamp)s);""",
-                {"ip": ip,
-                 "timestamp": self.get_timestamp()})
-        else:
-            # The ip already had a bad attempt to login
-            # increment the num of attempts in the db
-            cur.execute(
-                """UPDATE """ + self.table_name + """ SET attempts = attempts + 1, last_attempt = %(timestamp)s 
-                WHERE id = %(id)s;""",
-                {"id": row_id,
-                 "timestamp": self.get_timestamp()})
-        self.conn.commit()
+                cur.execute(
+                    """INSERT INTO """ + self.table_name + """ VALUES (DEFAULT, %(ip)s, 1, %(timestamp)s);""",
+                    {"ip": ip,
+                     "timestamp": self.get_timestamp()})
+            else:
+                # The ip already had a bad attempt to login
+                # increment the num of attempts in the db
+                cur.execute(
+                    """UPDATE """ + self.table_name + """ SET attempts = attempts + 1, last_attempt = %(timestamp)s 
+                    WHERE id = %(id)s;""",
+                    {"id": row_id,
+                     "timestamp": self.get_timestamp()})
+            self.conn.commit()
 
     # function notify login success(ip) - if ip previously failed to auth, we will delete its failed records (make it
     # clean)
@@ -75,7 +82,7 @@ class LoginGuard:
         row_id = self.get_id_by_ip(ip)
         if row_id is not None:
             cur.execute("""SELECT attempts, last_attempt from """ + self.table_name + """ WHERE id = %(id)s;""",
-                             {"id": row_id})
+                        {"id": row_id})
             attempts, last_attempt_datetime = cur.fetchone()
             attempt_delta = datetime.now() - last_attempt_datetime
             if attempt_delta.days >= self.ban_expire_days:
